d4aa8a521718a6c5aef0ab427dd36dcb9ac15aea
ClayGao <r700000@gmail.com>
2019-05-04 14:53:38 +0800

    'hw_week3_03_0504'

--- homeworks/week3/hw4.test.js
+++ homeworks/week3/hw4.test.js
@@ -4,4 +4,13 @@ describe('hw4', () => {
   it('should return correct answer when str = abcdcba', () => {
     expect(isPalindromes('abcdcba')).toBe(true);
   });
+
+  it('should return correct answer when str = abcdcba', () => {
+    expect(isPalindromes('CLay like to be ot ekil yalc')).toBe(false);
+  });
+
+  it('should return correct answer when str = abcdcba', () => {
+    expect(isPalindromes('CLay like to be ot ekil yalcCLay like to be ot ekil yalcCLay like to be ot ekil yalcCLay like to be ot ekil yalc'))
+      .toBe('字串長度需小於 100 個字元');
+  });
 });
--- homeworks/week3/hw5.js
+++ homeworks/week3/hw5.js
@@ -13,34 +13,37 @@ function add(a, b) {
       x.unshift('0');
     }
   }
-  const result = [];
-  let num = 0;
-  let addN = 0;
-  for (let i = x.length - 1; i >= 0; i -= 1) {
-    num = Number(x[i]) + Number(y[i]) + addN;
-    addN = 0;
 
-    if (num >= 10) {
-      if (i > 0) {
-        addN += 1;
-        num -= 10;
+  if (x.length >= 1 && x.length <= 1000) {
+    const result = [];
+    let num = 0;
+    let addN = 0;
+    for (let i = x.length - 1; i >= 0; i -= 1) {
+      num = Number(x[i]) + Number(y[i]) + addN;
+      addN = 0;
+
+      if (num >= 10) {
+        if (i > 0) {
+          addN += 1;
+          num -= 10;
+          result.unshift(num);
+        } else {
+          addN += 1;
+          num -= 10;
+          result.unshift(num);
+          result.unshift(addN);
+          console.log(result);
+        }
+      } else if (num < 10) {
         result.unshift(num);
-      } else {
-        addN += 1;
-        num -= 10;
-        result.unshift(num);
-        result.unshift(addN);
-        console.log(result);
       }
-    } else if (num < 10) {
-      result.unshift(num);
     }
-  }
-  let answer = '';
-  for (let i = 0; i < result.length; i += 1) {
-    answer += String(result[i]);
-  }
-  return answer;
+    let answer = '';
+    for (let i = 0; i < result.length; i += 1) {
+      answer += String(result[i]);
+    }
+    return answer;
+  } return '數字的長度需要大於等於 1 且不大於 1000 才行';
 }
 
 module.exports = add;
