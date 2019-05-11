e1dacb5596a84059b353a213020b1445a106ded0
ClayGao <r700000@gmail.com>
2019-05-11 20:50:30 +0800

    hw_week4_02_0511

--- homeworks/week4/hw2.js
+++ homeworks/week4/hw2.js
@@ -11,17 +11,8 @@ if (process.argv[2] === 'list') {
 
 
 if (process.argv[2] === 'read') {
-  // console.log (process.argv[3])
   request(`https://lidemy-book-store.herokuapp.com/books/${process.argv[3]}`, (error, response, body) => {
     const json = JSON.parse(body);
     console.log(json);
-    /*
-      const obj = json
-      for (var i = 0; i < obj.length; i++) {
-          if (obj[i].id === Number(process.argv[3])) {
-              console.log(obj[i])
-          }
-      }
-      */
   });
 }
--- homeworks/week4/hw3.js
+++ homeworks/week4/hw3.js
@@ -11,33 +11,21 @@ if (process.argv[2] === 'list') {
 
 
 if (process.argv[2] === 'read') {
-  // console.log (process.argv[3])
   request.get(`https://lidemy-book-store.herokuapp.com/books/${process.argv[3]}`, (error, response, body) => {
     const json = JSON.parse(body);
     console.log(json);
-    /*
-      const obj = json
-      for (var i = 0; i < obj.length; i++) {
-          if (obj[i].id === Number(process.argv[3])) {
-              console.log(obj[i])
-          }
-      }
-      */
   });
 }
 
 
 if (process.argv[2] === 'delete') {
-  // console.log (process.argv[3])
   request.delete(`https://lidemy-book-store.herokuapp.com/books/${process.argv[3]}`, (response) => {
-    // const json = JSON.parse(body)
     console.log(response.statusCode);
   });
 }
 
 
 if (process.argv[2] === 'create') {
-  // console.log (process.argv[3])
   request.post(
     {
       url: 'https://lidemy-book-store.herokuapp.com/books',
@@ -50,7 +38,6 @@ if (process.argv[2] === 'create') {
 
 
 if (process.argv[2] === 'update') {
-  // console.log (process.argv[3])
   request.patch(
     {
       url: `https://lidemy-book-store.herokuapp.com/books/${process.argv[3]}`,
